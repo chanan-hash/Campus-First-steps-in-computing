c66e31ccbcbf53cd0cece3566d2038dd96e9f9eb
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2021-08-15 20:45:50 +0300

    File learning practice

--- File_learning.py
+++ File_learning.py
@@ -1,7 +1,8 @@
 # This part is actually form the pre course self.py - begining with python
 
-#הפונקציה מקבלת כפרמטרים נתיבים של שני קבצי טקסט (מחרוזות).
-# הפונקציה מחזירה אמת (True) אם הקבצים זהים בתוכנם, אחרת מחזירה שקר (False).
+# The Func' gets as an argument two paths of files, as a string format
+# The Func' returns True if both files identical in their content, else returns False
+
 def are_files_equal(file1, file2):
     for i in file1:
         for j in file2:
@@ -9,7 +10,6 @@ def are_files_equal(file1, file2):
                 return True
             else:
                 return False
-# ניתן גם לפתוח ולקרוא את שניהם ע"י read ולבדוק האם הם שווים למשל:
 # if file2 in file1:
 
 # https://www.geeksforgeeks.org/python-filecmp-cmp-method/
@@ -28,21 +28,8 @@ def are_files_equal(file1, file2):
 
 file_object = open(r"C:\Users\חנן\Desktop\python\TestProject\NextPy\names.txt", "r")
 
-#בכדי לקרוא את תוכן הקובץ כולו ניעזר במתודה ()read.
-# המתודה מחזירה את תוכן הקובץ כולו כ מחרוזת אחת!!! שכוללת n\ בסוף כל שורה של הקובץ המקורי.
-
 #read_file = file_object.read()
 #print(read_file)
-
-#מה החיסרון בקריאת תוכן של קובץ בעזרת המתודה ()read?
-# הדוגמה לעיל מכילה קובץ טקסט קצרצר ובו ארבע שורות בלבד.
-# לעומת זאת, בעבודה עם קבצים גדולים, השמה של תוכן קובץ לתוך משתנה יחיד עלולה ליצור עומס בזיכרון ולהאט את ריצת התוכנית.
-# לפיכך המתודה ()read אינה מתאימה לקריאת קבצים גדולים
-
-#              שימוש בלולאה למעבר על השורות ועל תוכן הקובץ
-
-#בעבודה עם קבצים גדולים מומלץ להיעזר בשיטה אחרת: מעבר על שורה אחת בכל פעם, באמצעות לולאת for.
-# בצורה זו נוכל גם להדפיס את הקובץ שורה אחר שורה:
 
 # function that check the long string in the file, Iw've started it without putting it in a function, and then I have put it inside it.
 def long_string_in_file (file):
@@ -66,10 +53,6 @@ def long_string_in_file (file):
     print(z)
     print(n)
 
-# לא ניתן להשתמש פה במעבר ע"י אינדקס ע"י range(len(file_object))
-# כי נקבל:
-#TypeError: object of type '_io.TextIOWrapper' has no len()
-# זה לא מחרוזת
 long_string_in_file(file_object)
 
 #def short_string_in_file(file):
@@ -145,4 +128,3 @@ def who_is_missing(file_name):
     file_3 = open(r"found.txt","w")
     file_3.write("The missing number",j)
     file_3.close()
-
